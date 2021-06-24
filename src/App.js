f521974e97a9a85b34caa8e0a68bcbf93c44a393
aggutierrez <agustinguti123@gmail.com>
2021-06-24 12:08:07 -0300

    Agregando todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bienvenido</h1>
+        <p>A la diplomatura en React</p>
+        <p>En el curso se aprenderan los fundamentos para el desarrollo de aplicaciones con ReactJs</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Para mas informacion sobre ReactJs
         </a>
       </header>
     </div>
